f9ea6e3e659dcc8b0a78fff28120f9b1b2fcf1c3
thearon-laptop <thearon.helgeson@gmail.com>
2015-02-03 19:19:08 -0600

    move js to controller file

---
+++ js/controllers/siteController.js
@@ -0,0 +1,17 @@
+<script language="JavaScript" >
+    function siteController($scope) {
+        $scope.firstName = "",
+        $scope.lastName = ""
+        $scope.mySwitch = false;
+        $scope.baby = false;
+        $scope.toggle = function(show_hide) {
+            $scope.mySwitch = show_hide;
+            if ($scope.mySwitch === false){
+              $scope.showbaby(false);
+            }
+        };
+        $scope.showbaby = function(show_hide) {
+            $scope.baby = show_hide;
+        };
+    }
+  </script> 
