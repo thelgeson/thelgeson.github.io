85af8d0c6107c20c564655975309ac20484b025b
thearon-laptop <thearon.helgeson@gmail.com>
2015-02-03 19:43:41 -0600

    move stuff around

--- js/controllers/siteController.js
+++ js/controllers/siteController.js
@@ -1,17 +1,18 @@
-<script language="JavaScript" >
-    function siteController($scope) {
-        $scope.firstName = "",
-        $scope.lastName = ""
-        $scope.mySwitch = false;
-        $scope.baby = false;
-        $scope.toggle = function(show_hide) {
-            $scope.mySwitch = show_hide;
-            if ($scope.mySwitch === false){
-              $scope.showbaby(false);
-            }
-        };
-        $scope.showbaby = function(show_hide) {
-            $scope.baby = show_hide;
-        };
-    }
-  </script> 
+app.controller("myCtrl", 
+	function siteController($scope) {
+	    $scope.firstName = "",
+	    $scope.lastName = ""
+	    $scope.mySwitch = false;
+	    $scope.baby = false;
+	    $scope.toggle = function(show_hide) {
+	        $scope.mySwitch = show_hide;
+	        if ($scope.mySwitch === false){
+	          $scope.showbaby(false);
+	        }
+	    };
+	    $scope.showbaby = function(show_hide) {
+	        $scope.baby = show_hide;
+	    };
+});
+
+
